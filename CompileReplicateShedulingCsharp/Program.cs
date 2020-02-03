9815815cffbd678890b83afe36f52e367e710df4
Masud Rana <masudme09@gmail.com>
2020-02-03 17:53:27 +0600

    some changes

--- CompileReplicateShedulingCsharp/Program.cs
+++ CompileReplicateShedulingCsharp/Program.cs
@@ -15,7 +15,26 @@ namespace CompileReplicateShedulingCsharp
 
             //Algorithm based on deadline..all tasks are prioritize based on deadline
             //Sort targets smallest to largest based on deadlines
-            //Order list of dependencies based on new sorted target
+            //take the first target and get its dependencies.
+            //Take first dependencies and assigned that on first server, take second one compare time=completion time of the task running at first server +compilation of this task
+            //with time=compilation of this task + replication time..Do whatever smallest
+            //If all dependencies of fist target finished write on console first target done with time stamp
+
+            long clock = 0;
+            long compilationSteps = 0;
+            List<target> targets = input.targets;
+            targets = targets.OrderBy(x => x.deadline).ToList();
+            foreach(target target in targets)
+            {
+                string targetId = target.idOfTarget;
+                //Search with targetId on the files and get dependencies
+                compiledFiles compiledFileOnlist = input.compiledFiles.Find(x => x.id == targetId);
+
+                //Now get dependencies, inner dependent come first
+
+                
+            }
+
 
         }
     }
