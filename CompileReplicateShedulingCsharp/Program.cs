21079c5f154784c5caf31875b3c5710316345fe6
Masud Rana <masudme09@gmail.com>
2020-02-04 19:02:06 +0600

    Some changes but not stable

--- CompileReplicateShedulingCsharp/Program.cs
+++ CompileReplicateShedulingCsharp/Program.cs
@@ -13,85 +13,72 @@ namespace CompileReplicateShedulingCsharp
         static void Main(string[] args)
         {
             string filePath=Console.ReadLine();
-            Input input = new Input(filePath);
 
-            //Algorithm based on deadline..all tasks are prioritize based on deadline
-            //Sort targets smallest to largest based on deadlines
-            //take the first target and get its dependencies.
-            //Take first dependencies and assigned that on first server, take second one compare time=completion time of the task running at first server +compilation of this task
-            //with time=compilation of this task + replication time..Do whatever smallest
-            //If all dependencies of fist target finished write on console first target done with time stamp
+            output(filePath, false);
+            Console.WriteLine();
+            output(filePath, true);
 
-            /*Target
-             * -Get dependencies all
-             * -Processing 
-             * 
-             * 
-             * 
-             * 
-             */
 
-                               
-            
-            
-            long compilationSteps = 0;          
-            foreach (target target in input.targetsBasedOnDeadline)
-            {
-                listAllDependencies = new List<compiledFiles>();
-                listAllDependencies = listOfAllDependencies(target.targettedFile);
-                listAllDependencies.Reverse();
-                /*
-                 * Test code to check dependencies
-                    foreach(compiledFiles compiledFiles in listAllDependencies)
-                    {
-                        Console.WriteLine(target.targettedFile.id+":\n"+compiledFiles.id+"\n");
-                    }
-                */
-                //assign dependencies to server and complete the target
-                //Selected server to process this target
-                List<Server> sortedServers = input.servers.OrderBy(x =>x.serverProcessTime).ToList();
-                Server selectedServer = sortedServers[0];
+            //Input input = new Input(filePath);
 
-                foreach (compiledFiles dependent in listAllDependencies)
-                {
-                    if (dependent.compiled == false) //need to compile
-                    {
-                        if(dependent==target.targettedFile)
-                        {
-                            selectedServer.processFile(dependent);
-                        }
-                        else if(dependent.compilationTime<dependent.replicationTime)
-                        {
-                            selectedServer.processFile(dependent);
-                        }else if(dependent.compilationTime > dependent.replicationTime && sortedServers.Count>0)
-                        {
-                            sortedServers[1].processFile(dependent);
-                        }else
-                        {
-                            selectedServer.processFile(dependent);
-                        }
-                            
-                        compilationSteps++;
-                    }
-                    else //need to replicate or compile again..If compile again need to increase compilation steps
-                    {
-                        if(!selectedServer.processedFiles.Contains(dependent) && dependent.compilationTime<dependent.replicationTime)
-                        {
-                            selectedServer.processFile(dependent);
-                            compilationSteps++;
-                        }
-                    }
-                }               
-                
-                
-            }
+            //long compilationSteps = 0;          
+            //foreach (target target in input.targetsBasedOnDeadline)
+            //{
+            //    listAllDependencies = new List<compiledFiles>();
+            //    listAllDependencies = listOfAllDependencies(target.targettedFile);
+            //    listAllDependencies.Reverse();
+            //    /*
+            //     * Test code to check dependencies
+            //        foreach(compiledFiles compiledFiles in listAllDependencies)
+            //        {
+            //            Console.WriteLine(target.targettedFile.id+":\n"+compiledFiles.id+"\n");
+            //        }
+            //    */
+            //    //assign dependencies to server and complete the target
+            //    //Selected server to process this target
+            //    List<Server> sortedServers = input.servers.OrderBy(x =>x.serverProcessTime).ToList();
+            //    Server selectedServer = sortedServers[0];
 
-            Console.WriteLine(compilationSteps);
-            foreach(string line in Server.outputs)
-            {
-                Console.WriteLine(line);
-            }
-            
+            //    foreach (compiledFiles dependent in listAllDependencies)
+            //    {
+            //        if (dependent.compiled == false) //need to compile
+            //        {
+            //            if(dependent==target.targettedFile)
+            //            {
+            //                selectedServer.processFile(dependent);
+            //            }
+            //            else if(dependent.compilationTime<dependent.replicationTime)
+            //            {
+            //                selectedServer.processFile(dependent);
+            //            }else if(dependent.compilationTime > dependent.replicationTime && sortedServers.Count>0)
+            //            {
+            //                sortedServers[1].processFile(dependent);
+            //            }else
+            //            {
+            //                selectedServer.processFile(dependent);
+            //            }
+
+            //            compilationSteps++;
+            //        }
+            //        else //need to replicate or compile again..If compile again need to increase compilation steps
+            //        {
+            //            if(!selectedServer.processedFiles.Contains(dependent) && dependent.compilationTime<dependent.replicationTime)
+            //            {
+            //                selectedServer.processFile(dependent);
+            //                compilationSteps++;
+            //            }
+            //        }
+            //    }               
+
+
+            //}
+
+            //Console.WriteLine(compilationSteps);
+            //foreach(string line in Server.outputs)
+            //{
+            //    Console.WriteLine(line);
+            //}
+
             //Calculate Points
 
 
@@ -111,6 +98,77 @@ namespace CompileReplicateShedulingCsharp
             return listAllDependencies;
         }
 
+        public static void output(string filePath, bool basedOn)
+        {
+            Input input = new Input(filePath);
+            List<target> targets=new List<target>();
+            if(basedOn==true)
+            {
+                targets = input.targetsBasedOnDeadline;
+            }else
+            {
+                targets = input.targetsBasedOnGoalPoints;
+            }
+            long compilationSteps = 0;
+            foreach (target target in targets)
+            {
+                listAllDependencies = new List<compiledFiles>();
+                listAllDependencies = listOfAllDependencies(target.targettedFile);
+                listAllDependencies.Reverse();
+                /*
+                 * Test code to check dependencies
+                    foreach(compiledFiles compiledFiles in listAllDependencies)
+                    {
+                        Console.WriteLine(target.targettedFile.id+":\n"+compiledFiles.id+"\n");
+                    }
+                */
+                //assign dependencies to server and complete the target
+                //Selected server to process this target
+                List<Server> sortedServers = input.servers.OrderBy(x => x.serverProcessTime).ToList();
+                Server selectedServer = sortedServers[0];
+
+                foreach (compiledFiles dependent in listAllDependencies)
+                {
+                    if (dependent.compiled == false) //need to compile
+                    {
+                        if (dependent == target.targettedFile)
+                        {
+                            selectedServer.processFile(dependent);
+                        }
+                        else if (dependent.compilationTime < dependent.replicationTime)
+                        {
+                            selectedServer.processFile(dependent);
+                        }
+                        else if (dependent.compilationTime > dependent.replicationTime && sortedServers.Count > 0)
+                        {
+                            sortedServers[1].processFile(dependent);
+                        }
+                        else
+                        {
+                            selectedServer.processFile(dependent);
+                        }
+
+                        compilationSteps++;
+                    }
+                    else //need to replicate or compile again..If compile again need to increase compilation steps
+                    {
+                        if (!selectedServer.processedFiles.Contains(dependent) && dependent.compilationTime < dependent.replicationTime)
+                        {
+                            selectedServer.processFile(dependent);
+                            compilationSteps++;
+                        }
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine(compilationSteps);
+            foreach (string line in Server.outputs)
+            {
+                Console.WriteLine(line);
+            }
+        }
         
     }
 }
